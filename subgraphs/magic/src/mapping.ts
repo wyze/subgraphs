e9f57f8a026c69da54933f25b1ba8a5fb128273d
Neil Kistner <neil.kistner@gmail.com>
2022-10-30 16:40:09 -0500

    Add MAGIC subgraph

---
+++ subgraphs/magic/src/mapping.ts
@@ -0,0 +1,39 @@
+import { Address, BigDecimal } from "@graphprotocol/graph-ts";
+
+import { updateDaily, updateUserDaily } from "@shared/erc20";
+import { Direction, handleApproval, handleTransfer } from "@shared/helpers";
+
+import * as ERC20 from "../generated/MAGIC/ERC20";
+
+const DIVIDER = BigDecimal.fromString(`${1e18}`);
+
+export function onApproval(event: ERC20.Approval): void {
+  const params = event.params;
+
+  handleApproval(event, {
+    operator: params.operator,
+    owner: params.owner,
+    value: params.value.divDecimal(DIVIDER),
+  });
+}
+
+export function onTransfer(event: ERC20.Transfer): void {
+  const params = event.params;
+  const operator = event.transaction.to;
+
+  const amount = params.amount.divDecimal(DIVIDER);
+  const from = params.from;
+  const to = params.to;
+
+  handleTransfer(event, {
+    amount,
+    from,
+    operator: operator ? operator : Address.zero(),
+    to,
+    tokenId: null,
+  });
+
+  updateDaily(event, { amount, from, to });
+  updateUserDaily(event, { address: from, amount, direction: Direction.Out });
+  updateUserDaily(event, { address: to, amount, direction: Direction.In });
+}
